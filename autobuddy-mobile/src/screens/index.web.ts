eb34f3ce9ba35354230665e2249f301881b6759b
Dhanya <dhanya@example.com>
2026-06-04 19:55:13 +0530

    ijij

--- autobuddy-mobile/src/screens/index.web.ts
+++ autobuddy-mobile/src/screens/index.web.ts
@@ -2,13 +2,13 @@ import type { ComponentType } from 'react';
 
 import AdminDashboardRaw from './AdminDashboard';
 import AuthScreenRaw from './AuthScreen';
-import DriverDashboardWeb from './DriverDashboard.web';
+import DriverAcceptingPageWeb from './DriverAcceptingPage.web';
 import OperatorDashboardRaw from './OperatorDashboard';
 import PassengerMapWeb from './PassengerMap.web';
 import type { AuthScreenProps, RoleScreenProps } from './types';
 
 export const AuthScreen = AuthScreenRaw as ComponentType<AuthScreenProps>;
 export const PassengerMap = PassengerMapWeb as ComponentType<RoleScreenProps>;
-export const DriverDashboard = DriverDashboardWeb as ComponentType<RoleScreenProps>;
+export const DriverDashboard = DriverAcceptingPageWeb as ComponentType<RoleScreenProps>;
 export const OperatorDashboard = OperatorDashboardRaw as ComponentType<RoleScreenProps>;
 export const AdminDashboard = AdminDashboardRaw as ComponentType<RoleScreenProps>;
