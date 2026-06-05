0b3fd707bd839b94ff32aa2d53b53a6eefb27f46
Dhanya <dhanya@example.com>
2026-06-05 10:32:03 +0530

    ghfg

--- autobuddy-mobile/jest.setup.ts
+++ autobuddy-mobile/jest.setup.ts
@@ -1,1 +1,4 @@
 // Intentionally minimal test setup; avoid importing unavailable matchers.
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
